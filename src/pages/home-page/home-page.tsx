548cb43232b20f8466c36835f48825bcb6ad3a41
chliyang <chengli.yang@thoughtworks.com>
2022-05-05 23:47:32 +0800

    feat: add logout in home page

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,14 +1,29 @@
+import { Button } from "antd";
 import React from "react";
 import { logout } from "../../utils/session";
+import styles from "./home-page.module.scss";
 
 const HomePage = (props: any) => {
-  const { value } = props;
+  const { history } = props;
+
+  const handleClick = () => {
+    logout();
+    history.push("/login");
+    window.location.reload();
+  };
+
   return (
-    <div>
-      <h1>hi</h1>
-      <h2>hi</h2>
-      <button onClick={() => value.history.push("/login")}>tiaozhuan</button>
-      <button onClick={logout}>退出</button>
+    <div className={styles.container}>
+      <header className={styles.header}>The Home Page</header>
+      <Button
+        className={styles.bottom}
+        onClick={handleClick}
+        size="large"
+        type="primary"
+        block
+      >
+        退出登录
+      </Button>
     </div>
   );
 };
